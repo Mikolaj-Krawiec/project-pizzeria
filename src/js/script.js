f32ebe0552370136111965250113c07f41b18b2c
Mikolaj-Krawiec <59422679+Mikolaj-Krawiec@users.noreply.github.com>
2020-03-25 23:09:08 +0100

    Add active class to img in processOrder()

--- src/js/script.js
+++ src/js/script.js
@@ -76,6 +76,8 @@
     getElements(){
       const thisProduct = this;
 
+      this.imageWrapper = this.element.querySelector(select.menuProduct.imageWrapper);
+
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
@@ -119,7 +121,16 @@
       let price = dataSource.products[this.id].price;
       for( let param in dataSource.products[this.id].params) {
         for(let option in dataSource.products[this.id].params[param].options) {
+          // Add active class to image
+          const img = this.imageWrapper.querySelector(`.${param}-${option}`);
+          if(img){
+            img.classList.remove(classNames.menuProduct.imageVisible);
+          }
           if(formData[param]){
+            if(formData[param].includes(option) && img) {
+              img.classList.add(classNames.menuProduct.imageVisible);
+            }
+            // END Add active class to image
             if(formData[param].includes(option) && !dataSource.products[this.id].params[param].options[option].default) {
               price += dataSource.products[this.id].params[param].options[option].price;
             } else if(!formData[param].includes(option) && dataSource.products[this.id].params[param].options[option].default) {
@@ -128,6 +139,7 @@
           } else if(dataSource.products[this.id].params[param].options[option].default) {
             price -= dataSource.products[this.id].params[param].options[option].price;
           }
+
         }
       }
       this.element.querySelector('.price').innerHTML = price;
