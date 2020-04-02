83c8a676789fb707b89334df481a2f329f03d1b9
Mikolaj-Krawiec <59422679+Mikolaj-Krawiec@users.noreply.github.com>
2020-04-02 23:36:20 +0200

    Fix processOrder && Add sendOrder() in Cart

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@
   const select = {
     templateOf: {
       menuProduct: '#template-menu-product',
-      cartProduct: '#template-cart-product', // CODE ADDED
+      cartProduct: '#template-cart-product',
     },
     containerOf: {
       menu: '#product-list',
@@ -27,12 +27,11 @@
     },
     widgets: {
       amount: {
-        input: 'input.amount', // CODE CHANGED
+        input: 'input.amount',
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
       },
     },
-    // CODE ADDED START
     cart: {
       productList: '.cart__order-summary',
       toggleTrigger: '.cart__summary',
@@ -51,7 +50,6 @@
       edit: '[href="#edit"]',
       remove: '[href="#remove"]',
     },
-    // CODE ADDED END
   };
 
   const classNames = {
@@ -59,11 +57,9 @@
       wrapperActive: 'active',
       imageVisible: 'active',
     },
-    // CODE ADDED START
     cart: {
       wrapperActive: 'active',
     },
-    // CODE ADDED END
   };
 
   const settings = {
@@ -71,19 +67,20 @@
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }, // CODE CHANGED
-    // CODE ADDED START
+    },
     cart: {
       defaultDeliveryFee: 20,
     },
-    // CODE ADDED END
+    db: {
+      url: '//localhost:3131',
+      product: 'product',
+      order: 'order',
+    },
   };
 
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
-    // CODE ADDED START
     cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
-    // CODE ADDED END
   };
 
   class Product {
@@ -154,36 +151,41 @@
     processOrder() {
       const formData = utils.serializeFormToObject(this.form);
       this.params = {};
-      this.price = dataSource.products[this.id].price;
-      for( let param in dataSource.products[this.id].params) {
-        for(let option in dataSource.products[this.id].params[param].options) {
-          // Add active class to image
-          const img = this.imageWrapper.querySelector(`.${param}-${option}`);
-          if(img){
-            img.classList.remove(classNames.menuProduct.imageVisible);
-          }
-          if(formData[param]){
-            if(formData[param].includes(option) && img) {
-              img.classList.add(classNames.menuProduct.imageVisible);
-            }
-            // Add option to this.parms
-            if(!this.params[param]) {
+      for(const products of app.data.products ) {
+        if(products.id == this.id) {
+          this.price = products.price;
+          for( let param in products.params) {
+            if(formData[param]) {
               this.params[param] = {
-                label: dataSource.products[this.id].params[param].label,
+                label: products.params[param].label,
                 options: {},
               };
             }
-            this.params[param].options[option] = dataSource.products[this.id].params[param].options[option].label;
-            // Adding price of checked option to price
-            if(formData[param].includes(option) && !dataSource.products[this.id].params[param].options[option].default) {
-              this.price += dataSource.products[this.id].params[param].options[option].price;
-            } else if(!formData[param].includes(option) && dataSource.products[this.id].params[param].options[option].default) {
-              this.price -= dataSource.products[this.id].params[param].options[option].price;
+            for(let option in products.params[param].options) {
+              // Add active class to image
+              const img = this.imageWrapper.querySelector(`.${param}-${option}`);
+              if(img){
+                img.classList.remove(classNames.menuProduct.imageVisible);
+              }
+              if(formData[param]){
+                if(formData[param].includes(option) && img) {
+                  img.classList.add(classNames.menuProduct.imageVisible);
+                }
+                // Add option to this.parms
+                if(formData[param].includes(option)) {
+                  this.params[param].options[option] = products.params[param].options[option].label;
+                }
+                // Adding price of checked option to price
+                if(formData[param].includes(option) && !products.params[param].options[option].default) {
+                  this.price += products.params[param].options[option].price;
+                } else if(!formData[param].includes(option) && products.params[param].options[option].default) {
+                  this.price -= products.params[param].options[option].price;
+                }
+              } else if(products.params[param].options[option].default) {
+                this.price -= products.params[param].options[option].price;
+              }
             }
-          } else if(dataSource.products[this.id].params[param].options[option].default) {
-            this.price -= dataSource.products[this.id].params[param].options[option].price;
           }
-
         }
       }
       this.priceSingle = this.price;
@@ -266,6 +268,9 @@
       this.dom.productList = document.querySelector(select.cart.productList);
       this.dom.wrapper = element;
       this.dom.toggleTrigger = this.dom.wrapper.querySelector(select.cart.toggleTrigger);
+      this.dom.form = this.dom.wrapper.querySelector(select.cart.form);
+      this.dom.phone = this.dom.wrapper.querySelector(select.cart.phone);
+      this.dom.address = this.dom.wrapper.querySelector(select.cart.address);
 
       this.renderTotalsKeys = ['totalNumber', 'totalPrice', 'subtotalPrice', 'deliveryFee'];
       for(let key of this.renderTotalsKeys){
@@ -300,6 +305,46 @@
       );
       this.dom.productList.addEventListener('updated' , this.update.bind(this));
       this.dom.productList.addEventListener('remove' , () => {this.remove(event.detail.cartProduct);});
+      this.dom.form.addEventListener('submit', ()=> {
+        event.preventDefault();
+        this.sendOrder();
+      });
+    }
+
+    sendOrder() {
+      if(this.products.length != 0 && this.dom.address.value && this.dom.phone.value) {
+        const url = settings.db.url + '/' + settings.db.order;
+
+        const payload = {
+          address: this.dom.address.value,
+          phone: this.dom.phone.value,
+          subtotalPrice: this.subtotalPrice,
+          totalPrice: this.totalPrice,
+          totalNumber: this.totalNumber,
+          deliveryFee: this.deliveryFee,
+          products: [],
+        };
+
+        for(const product of this.products) {
+          payload.products.push(product.getData());
+        }
+
+        const option = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(payload),
+        };
+
+        fetch(url, option)
+          .then(respond => respond.json())
+          .then(parsedRespond => {
+            console.log('parsedRespond: ', parsedRespond);
+          });
+      } else {
+        alert('Wrong input');
+      }
     }
 
     remove(cartProduct) {
@@ -332,6 +377,16 @@
       this.getElements(element);
       this.initAmountWidget();
       this.initActions();
+    }
+
+    getData() {
+      return {
+        id: this.id,
+        amount: this.amount,
+        price: this.price,
+        priceSingle: this.priceSingle,
+        params: this.options,
+      };
     }
 
     getElements(element) {
@@ -387,12 +442,25 @@
 
     initMenu: function() {
       for(let productData in this.data.products) {
-        new Product(productData, this.data.products[productData]);
+        //new Product(productData, this.data.products[productData]);
+        new Product(this.data.products[productData].id, this.data.products[productData]);
       }
     },
 
     initData: function() {
-      this.data = dataSource;
+      this.data = {products: ''};
+      const url = settings.db.url + '/' + settings.db.product;
+      fetch(url)
+        .then((rawResponse) => {
+          return rawResponse.json();
+        })
+        .then((parsedResponse) => {
+          console.log('(parsedRespond:' ,(parsedResponse));
+          this.data.products = parsedResponse;
+          this.initMenu();
+        });
+
+      console.log('this.data', JSON.stringify(this.data));
     },
 
     init: function(){
@@ -404,7 +472,6 @@
       console.log('templates:', templates);
 
       this.initData();
-      this.initMenu();
       this.initCart();
     },
   };
